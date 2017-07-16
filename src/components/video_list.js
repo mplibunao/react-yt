657804b2b281eafb9d3fa33978c39a15e34c22e4
mplibunao <markpaololibunao@gmail.com>
2017-07-16 21:13:06 +0800

    Uggh I was using git add -u and didn't add the components folder :/

---
+++ src/components/video_list.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = ({videos, onVideoSelect})=>{
+  
+  const videoItems = videos.map(video => {
+    return (
+      <VideoListItem
+        key={video.etag}
+        video={video}
+        onVideoSelect={onVideoSelect}
+      />
+    );
+  });
+
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul> 
+  );
+}
+
+export default VideoList;
